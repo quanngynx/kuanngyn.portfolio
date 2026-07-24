8111204fd7a563410e6d99e4c511b3c027c8d615
quan3473 <nguyenminhquan042004@gmail.com>
2026-07-24 17:20:23 +0700

    feat: add new database and project references, update Prettier configuration, enhance global styles, and implement sitemap generation for improved SEO

---
+++ portfolio/src/app/[locale]/blog/sitemap.ts
@@ -0,0 +1,38 @@
+import { BASE_URL, BLOG_PATH } from '@/common/constants';
+import { getAllContent } from '@/common/providers/provider';
+import { MetadataRoute } from 'next';
+
+const GOOGLE_SITEMAP_LIMIT = 50_000;
+
+export async function generateSitemaps() {
+  const data = await getAllContent();
+  const sitemapCount = Math.ceil(data.length / GOOGLE_SITEMAP_LIMIT);
+  const sitemapIds = Array.from({ length: sitemapCount }, (_, id) => ({
+    id,
+  }));
+
+  return sitemapIds;
+}
+
+export default async function sitemap({
+  id,
+}: {
+  id: number;
+}): Promise<MetadataRoute.Sitemap> {
+  const data = await getAllContent();
+
+  const start = id * GOOGLE_SITEMAP_LIMIT;
+  const end = start + GOOGLE_SITEMAP_LIMIT;
+  const content = data.slice(start, end);
+
+  const contentUrls = content.map(
+    (content) =>
+      ({
+        url: `${BASE_URL}${BLOG_PATH}/${content.slug}`,
+        lastModified: content.date,
+        changeFrequency: 'daily',
+      }) as const
+  );
+
+  return contentUrls;
+}
